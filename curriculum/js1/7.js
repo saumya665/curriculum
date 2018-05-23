b25bce4b709cf41fa4810a2e90ffd9b6c487cd5b
Saumya Singh <saumyasing@gmail.com>
2018-05-23 15:25:06 -0700

    find prime numbers

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,12 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (num, i=2) => {
+  if (i === num) { return true}
+  if (num%i === 0){
+    return false;
+  }
+  return solution (num, i+1);
 };
 
 module.exports = {
