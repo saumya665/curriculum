23b7e65062de55503c8ac3351205b64f138e22d1
Saumya Singh <saumyasing@gmail.com>
2018-05-24 14:53:14 -0700

    return largest from an array

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -4,8 +4,14 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, i=0, largest=a[0]) => {
+  if (i===a.length) {
+    return largest;
+  }
+  if (a[i]>largest){
+    largest = a[i];
+  }
+  return solution(a,i+1, largest);
 };
 
 module.exports = {
