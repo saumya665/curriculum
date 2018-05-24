db60d7828a64b318f77fbb95164c92fda37493be
Saumya Singh <saumyasing@gmail.com>
2018-05-24 14:46:29 -0700

    array wo/prime

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -5,9 +5,28 @@
  * @param {array} a
  * @returns {array}
  */
+ const isPrime=(num,i=2)=>{
+    if(num<2){
+      return false;
+    }
+    if (i === num) {
+      return true
+    }
+    if(num%i===0){
+      return false;
+    }
+    return isPrime(num, i+1);
+  };
 
-const solution = (a) => {
-  return [];
+
+const solution = (a,i=0) => {
+  if (i===a.length){
+      return a;
+  }
+  if (isPrime(a[i])===true) {
+     a[i]=1;
+  };
+  return solution(a,i+1);
 };
 
 module.exports = {
