5e7af4951f64eda6d9719facaed3c35ee039f7e7
Saumya Singh <saumyasing@gmail.com>
2018-05-25 12:08:39 -0700

    find max of an array

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -4,11 +4,21 @@
  * @returns {number} max
  */
 
+const MaxValue=(arr,i=0, max=arr[0])=>{
+  if (i === arr.length) {
+    return max;
+  }
+  if (arr[i]>max) {
+    max = arr[i];
+  }
+  return MaxValue(arr, i+1, max);
+}
+
 const solution = (obj)=>{
-  return 0;
+  values= Object.values(obj); //returns array of values of object
+  return MaxValue(values);
 }
 
 module.exports = {
   solution
 }
-
