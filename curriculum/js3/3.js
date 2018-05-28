be879c77881aedbab043b9fcbfe5b7d13ffb0241
Saumya Singh <saumyasing@gmail.com>
2018-05-28 14:37:34 -0700

    find duplicates of an array

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -4,10 +4,24 @@
  * @return {array} b
  */
 
-const solution = (a)=>{
-  return b;
-}
+const findDuplicates=(obj,keys=Object.keys(obj), values=Object.values(obj), i=0, newArr=[])=>{
+    if( i === values.length ) { return newArr };
+    if (values[i] === 2) {
+      newArr.push(keys[i]);
+    }
+  return findDuplicates(obj, keys, values, i+1, newArr);
+  };
+
+
+const solution = (arr, i=0, obj={})=>{
+  // create an object from an array
+  if (i === arr.length){
+    return findDuplicates(obj);
+  }
+  !obj[arr[i]] ? obj[arr[i]] = 1 : obj[arr[i]] +=1;
+  return solution(arr, i+1, obj);
+};
 
 module.exports = {
   solution
-}
+};
