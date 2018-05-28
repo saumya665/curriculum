c173d71125ebf0922dcc1218cd76ccae7e8b0f85
Saumya Singh <saumyasing@gmail.com>
2018-05-28 13:28:29 -0700

    return most frequent number from an array

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -4,8 +4,19 @@
  * @return {number}
  */
 
-const solution = (arr)=>{
-  return 0;
+
+const indexMax=(obj)=>{
+  //return the number with largest max from object
+  keys=Object.keys(obj);
+  values=Object.values(obj);
+ return keys[values.indexOf(Math.max(...values))];
+};
+
+const solution = (arr, i=0, mostCom={})=>{
+  if (i === arr.length) return indexMax(mostCom);
+  !mostCom[arr[i]] ? mostCom[arr[i]] = 1 : mostCom[arr[i]] += 1;
+  return solution(arr, i+1, mostCom);
+  //populate object from the array
 };
 
 module.exports = {
